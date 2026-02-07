161e43919f52c925a103a47719894f37b38fae44
pranav <pranavsuresh107@gmail.com>
2026-02-07 19:19:28 +0530

    Finally managed to get the json input sanitization fully working

--- password_checker.c
+++ password_checker.c
@@ -215,19 +215,28 @@ void tojson(int score,char *password,int errorcode){
 	}
 
 	//create a copy of password
-	char copy[strlen(password)+2048];
-	for(int i =0;i<strlen(copy);i++){
-		if(copy[i] == '"'){
-			copy[i] = '\\';
-			copy[i+1] = '"';
-			i++;
-		}else if(copy[i] == '\\'){
-			copy[i] = '\\';
-			copy[i+1] = '\\';
-			i++;
+	char copy[strlen(password)];
+	strcpy(copy,password);
+	//new buffer for escaped string
+	//copy x 2 cus just in case every character is / and causes double // everywhere and +1 for \0
+	char buff[strlen(copy)*2+1];
+
+	//input sanitization logic
+	int j = 0;
+	for (int i = 0; copy[i] != '\0'; i++) {
+		if (copy[i] == '"' || copy[i] == '\\') {
+			//THIS is very important there is a difference between j++ and ++j if the below statment had ++j the 
+			//variable j would have updated first and then written the value one step ahead of the intended
+			//index but since it is j++ the below expression/line is processed aka the value is assigned to 
+			//buff at the required index and then only it increments the var j
+			//This is basic but i had a hard time and if u ever stumble on to this code please keep this in mind
+			//before changing the input sanitization of json
+			buff[j++] = '\\';
 		}
+		buff[j++] = copy[i];
 	}
+	buff[j] = '\0';
 
 	//directly printing everyting to stdout
-	printf("{\n\t\"password\" : \"%s\",\n\t\"score\" : %d,\n\t\"strength\" : \"%s\",\n\t\"length\" : %d,\n\t\"password_in_file\" : %s\n}\n",password,score,strength,plen,password_in_file);
+	printf("{\n\t\"password\" : \"%s\",\n\t\"score\" : %d,\n\t\"strength\" : \"%s\",\n\t\"length\" : %d,\n\t\"password_in_file\" : %s\n}\n",copy,score,strength,plen,password_in_file);
 }
